a9da34e7a58d6b668b31212f59cdda9870eb21d3
Kartik Tyagi <23mca063@jssaten.ac.in>
2025-07-15 19:16:47 +0530

    added Signup and signin routes

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { userRouter } from './routes/user'
+
+const app = new Hono()
+
+app.use('/*', cors())
+
+// app.route('/thinky', generateRouter)
+app.route('/thinky', userRouter)
+
+export default app
